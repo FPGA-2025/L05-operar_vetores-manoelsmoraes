4940066dbe595a211f595087a4b1e268f842e609
manoelsmoraes <manoel@msminfo.com.br>
2025-04-07 19:54:20 -0300

    Vetores OK

--- operar_vetores.v
+++ operar_vetores.v
@@ -6,4 +6,9 @@ module operar_vetores(
     output [5:0] saida_not
 );
 	//insira seu código aqui
+
+assign saida_or_bit_a_bit = a | b;              // OR bit a bit
+    assign saida_or_logico = (a != 3'b000) || (b != 3'b000);  // OR lógico
+    assign saida_not = ~{b, a};                     // NOT sobre vetor concatenado	
+	
 endmodule
